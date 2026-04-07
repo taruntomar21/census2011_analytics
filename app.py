f815ec0f8524a26e6dd9d9ad9868c46a273f8c23
Tarun Tomar <tarunkumar0707@icloud.com>
2026-04-07 19:39:04 +0530

    Do some changes in app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import streamlit as st
 import numpy as np
-import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import plotly.express as px
 from DB import DataBase
